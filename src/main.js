f874484622dc144cdda8120f902ec0ffd45c8011
Justin <jforlenza@steamcenter.nyc>
2026-05-28 14:56:24 -0400

    addded upgrades

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
-let score = 0
+let score = 99
 
 let clicker = document.getElementById("clicker")
 
 let scoreDisplay = document.getElementById("scoreDisplay")
 
+function updateScore (amount) {
+  score += amount
+  scoreDisplay.innerText = score.toFixed(0) + ' rpm' 
+}
+
 clicker.addEventListener("click", function () {
-  score++
-  scoreDisplay.innerText = score + ' rpm' 
+  let clickAmt = 1 + (cylinders ** 1.05)
+
+  updateScore(clickAmt)
 })
 
+let upgradeOne = document.getElementById("upgradeOne")
+let upgradeOneCount = document.getElementById("upgradeOneCount")
+let cylinders = 0
 
-
-
+upgradeOne.addEventListener("click", function () {
+  if (score >= 100) {
+     updateScore(-100)
+     cylinders++
+     upgradeOneCount.innerText = cylinders + " Cylinders"
+  } else {
+    alert('YOU BROKE')
+  }
+})
